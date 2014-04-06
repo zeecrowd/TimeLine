d31c8b55a561786271a39777b8f82e53402666e5
Pillow <zeecrowd@outlook.com>
2014-04-07 01:28:42 +0200

    Update readme and header
    Particularly the licensing

--- Build/build_TimeLine.cmd
+++ Build/build_TimeLine.cmd
@@ -5,7 +5,7 @@ set RCC= C:\Qt\Qt5.2.0\5.2.0\msvc2010\bin\rcc.exe
 set SRC=..\
 set OUTPUT=..\Deploy
 
-if not exist %OUTPUT%\. md %OUTPUT%
+IF NOT EXIST %OUTPUT%\. md %OUTPUT%
 
 copy %SRC%\%NAME%.cfg %OUTPUT%
-%RCC% -threshold 70 -binary -o %OUTPUT%/%NAME%.rcc %SRC%/%NAME%.qrc
+%RCC% -threshold 70 -binary -o %OUTPUT%\%NAME%.rcc %SRC%\%NAME%.Debug.generated.qrc
